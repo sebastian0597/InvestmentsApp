036e4b9e1c06dbb43eeba7b4bc93641c142933af
Omar Ibañez <omaribanez100@gmail.com>
2022-03-11 09:16:53 -0500

    Primer commit con la configuración inicial del proyecto

---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <script src="{{ asset('js/app.js')}}" defer></script>
+</head>
+<body>
+    <div id="app">
+        <div class="container">
+            <input type="text" class="datepicker" />
+        </div>
+    </div>
+  
+</body>
+</html>
