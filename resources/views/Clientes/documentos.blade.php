126b0797935baf94b72488d31ce3190f80d48a1a
Omar Ibanez <omaribanez100@gmail.com>
2022-06-03 19:34:50 -0500

    .

--- resources/views/Clientes/documentos.blade.php
+++ resources/views/Clientes/documentos.blade.php
@@ -117,15 +117,6 @@
 
         </div>
         <!-- footer start-->
-        <footer class="footer">
-          <div class="container-fluid">
-            <div class="row">
-              <div class="col-md-12 footer-copyright text-center">
-                <p class="mb-0">Copyright 2022 © </p>
-              </div>
-            </div>
-          </div>
-        </footer>
       </div>
     </div>
 @section('scripts')
--- resources/views/Clientes/inversiones.blade.php
+++ resources/views/Clientes/inversiones.blade.php
@@ -101,15 +101,7 @@
           <!-- Container-fluid Ends-->
         </div>
         <!-- footer start-->
-        <footer class="footer">
-          <div class="container-fluid">
-            <div class="row">
-              <div class="col-md-12 footer-copyright text-center">
-                <p class="mb-0">Copyright 2022 © </p>
-              </div>
-            </div>
-          </div>
-        </footer>
+
       </div>
     </div>
 @stop
