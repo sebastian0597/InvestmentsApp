d5eba40c933ecf19c3ad3b7f2081d51e9b351067
Omar Ibañez <omaribanez100@gmail.com>
2022-06-22 09:38:17 -0500

    mejoras en includes

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -74,7 +74,6 @@
     <script src="{{ asset('js/util.js') }}" defer></script> 
     <script src="{{ asset('js/admin/clientes.js') }}" defer></script>  
     <script src="{{ asset('js/admin/admins.js') }}" defer></script>  
-    <script src="{{ asset('js/admin/extractos.js') }}" defer></script>  
     <script src="{{ asset('js/admin/inversiones.js') }}" defer></script>
     <script src="{{ asset('js/admin/desembolsos.js') }}" defer></script>
     <script src="{{ asset('js/admin/kpis.js') }}" defer></script>
